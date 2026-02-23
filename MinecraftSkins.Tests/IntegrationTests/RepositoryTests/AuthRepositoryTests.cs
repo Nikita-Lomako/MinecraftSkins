1ce930a465500389bd9a8dfbd2127d615f06202d
Nikita Lomako <nikitich-2006@mail.ru>
2026-02-23 12:30:12 +0300

    Added: Project structure for automated testing with unit, integration, functional and E2E test layers

---
+++ MinecraftSkins.Tests/IntegrationTests/RepositoryTests/AuthRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftSkins.Tests.IntegrationTests.RepositoryTests
+{
+    internal class AuthRepositoryTests
+    {
+    }
+}
